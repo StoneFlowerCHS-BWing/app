68aea88cbccb611f0e9657432543f6dba9ac85b5
stonefllowerchs-bwing <stoneflowerchs.bwing@gmail.com>
2017-04-14 23:21:34 +0530

    Added menu support

--- Scripts/app.js
+++ Scripts/app.js
@@ -1,1 +1,28 @@
-angular.module("app", []);
+angular.module('app').config(function($routeProvider, $locationProvider) {
+    $routeProvider
+
+        .when('/', {
+            templateUrl: 'index.html',
+            controller: 'homeController'
+        })
+        .when('/ApprovedPlan1', {
+            templateUrl: 'StaticPage/ApprovedPlan1.html',
+            controller: 'homeController'
+        })
+        .when('/ApprovedPlan2', {
+            templateUrl: 'StaticPage/ApprovedPlan2.html',
+            controller: 'homeController'
+        })
+        .when('/CommencementCertificate', {
+            templateUrl: 'StaticPage/CommencementCertificate.html',
+            controller: 'homeController'
+        })
+        .when('/CompletionCertificate', {
+            templateUrl: 'StaticPage/CompletionCertificate.html',
+            controller: 'homeController'
+        })
+        .otherwise({
+            redirectTo: '/'
+        });
+    $locationProvider.html5Mode(true);
+});
