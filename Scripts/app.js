e6441b1eff7ef67fe13566f4f77931390250f0f5
stonefllowerchs-bwing <stoneflowerchs.bwing@gmail.com>
2017-04-16 06:41:34 +0530

    Checking by adding path

--- Scripts/app.js
+++ Scripts/app.js
@@ -4,7 +4,7 @@ myApp.config(['$routeProvider', '$locationProvider',
     function($routeProvider, $locationProvider) {
         $routeProvider
             .when('/', {
-                templateUrl: 'StaticPage/SocietyRegistration.html',
+                templateUrl: 'app/StaticPage/SocietyRegistration.html',
                 controller: 'homeController'
             })
             .when('/ApprovedPlan1', {
